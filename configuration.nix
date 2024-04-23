42a8fccfa616d89ab585e616e62ec321741c40cb
as3ii <as3ii777@gmail.com>
2024-04-23 22:45:13 +0200

    Fixed btrbk permissions

--- configuration.nix
+++ configuration.nix
@@ -40,6 +40,33 @@ in
     emulateWheel = true;
   };
 
+  # required for btrbk
+  security.sudo = {
+    enable = true;
+    extraRules = [{
+      commands = [
+        {
+          command = "${pkgs.coreutils-full}/bin/test";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${pkgs.coreutils-full}/bin/readlink";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${pkgs.btrfs-progs}/bin/btrfs";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+      users = [ "btrbk" ];
+    }];
+    extraConfig = with pkgs; ''
+      Defaults:picloud secure_path="${lib.makeBinPath [
+        btrfs-progs coreutils-full
+      ]}:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+    '';
+  };
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot = {
     enable = true;
