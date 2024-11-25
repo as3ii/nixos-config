b7f53f2034b21bb06b4ff33953697a4466a8203b
as3ii <as3ii777@gmail.com>
2024-11-25 10:37:47 +0100

    switched stable branch from 24.05 to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.follows = "nixpkgs-unstable";
 
