d343265198e8d83075fbee42397e649f2fb126a5
as3ii <as3ii777@gmail.com>
2025-02-26 10:53:22 +0100

    Added libvirt module

---
+++ modules/libvirt.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+{
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd
+        ];
+      };
+    };
+  };
+
+  programs.virt-manager.enable = true;
+}
+
+# vim: sw=2
