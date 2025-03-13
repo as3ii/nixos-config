3a2bffd144acbf73ff1ff3849182a85a807fc461
as3ii <as3ii777@gmail.com>
2025-03-13 14:34:39 +0100

    Reformatted podman module

--- modules/podman.nix
+++ modules/podman.nix
@@ -1,12 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
-  virtualisation = {
-    podman = {
-      enable = true;
-      dockerCompat = true;
-      defaultNetwork.settings.dns_enabled = true;
-    };
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
   };
 
   environment.systemPackages = with pkgs; [
