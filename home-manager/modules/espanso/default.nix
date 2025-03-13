c4897f37b69d7cd79eebf63bb64488f8bbb0debd
as3ii <as3ii777@gmail.com>
2025-03-13 14:35:31 +0100

    Improved/refactored espanso home-manager module

--- home-manager/modules/espanso/default.nix
+++ home-manager/modules/espanso/default.nix
@@ -1,39 +1,55 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
+  cfg = config.espanso;
   lang = config.services.xserver.xkb.layout or config.console.keyMap or "us";
-in
-{
-  services.espanso = {
-    enable = true;
-    #package = pkgs.espanso;
-    package = pkgs.espanso-wayland;
-    configs = {
-      default = {
-        keyboard_layout = {
-          rules = "evdev";
-          model = "pc105";
-          layout = "${lang}";
-        };
-        toggle_key = "RIGHT_ALT";
-        search_shortcut = "ALT+SHIFT+SPACE";
-      };
-    };
-    matches = {
-      #base = builtins.fromYAML (builtins.readFile ./base.yml);
-    };
-  };
-
-  xdg.configFile."espanso/match/base.yml".source = ./base.yml;
-  xdg.configFile."espanso/match/packages" = {
-    source = ./packages;
-    recursive = true;
-  };
-
-  home.packages = with pkgs; [
-    #xdotool
-    #xsel
+  pkg-wayland = with pkgs; [
     wl-clipboard
     ydotool
   ];
+  pkg-xorg = with pkgs; [
+    xdotool
+    xsel
+  ];
+in
+{
+  options.espanso = {
+    wayland = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      example = false;
+      description = ''
+        Enable wayland support (disables Xorg support).
+      '';
+    };
+  };
+
+  config = {
+    services.espanso = {
+      enable = true;
+      package = if cfg.wayland then pkgs.espanso-wayland else pkgs.espanso;
+      configs = {
+        default = {
+          keyboard_layout = {
+            rules = "evdev";
+            model = "pc105";
+            layout = "${lang}";
+          };
+          toggle_key = "RIGHT_ALT";
+          search_shortcut = "ALT+SHIFT+SPACE";
+        };
+      };
+      matches = {
+        #base = builtins.fromYAML (builtins.readFile ./base.yml);
+      };
+    };
+
+    xdg.configFile."espanso/match/base.yml".source = ./base.yml;
+    xdg.configFile."espanso/match/packages" = {
+      source = ./packages;
+      recursive = true;
+    };
+
+    home.packages = if cfg.wayland then pkg-wayland else pkg-xorg;
+  };
 }
