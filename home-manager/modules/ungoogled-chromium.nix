e21d9f2186c9eab89cad406d5fb9fa5423600fed
as3ii <as3ii777@gmail.com>
2025-03-14 15:48:00 +0100

    Commented non-working part of the ungoogled-chromium home-manager module

--- home-manager/modules/ungoogled-chromium.nix
+++ home-manager/modules/ungoogled-chromium.nix
@@ -4,9 +4,10 @@
   programs.chromium = {
     enable = true;
     package = pkgs.ungoogled-chromium;
-    extensions = [
-      { id = "ddkjiahejlhfcafbddmgiahcphecmpfh"; } # ublock origin light
-      { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # dark reader
-    ];
+    # disabled because this only works for normal chromium
+    #extensions = [
+    #  { id = "ddkjiahejlhfcafbddmgiahcphecmpfh"; } # ublock origin light
+    #  { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # dark reader
+    #];
   };
 }
