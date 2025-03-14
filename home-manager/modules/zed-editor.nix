925702943309e86268c6a888eb61458f0955c58f
as3ii <as3ii777@gmail.com>
2025-03-14 15:49:21 +0100

    Updated and improved zed-editor home-manager module

--- home-manager/modules/zed-editor.nix
+++ home-manager/modules/zed-editor.nix
@@ -3,20 +3,99 @@
 {
   programs.zed-editor = {
     enable = true;
-    extensions = [ "nix" ];
+    extensions = [ "nix" "ruff" "toml" ];
     extraPackages = with pkgs; [
-      #ruff
+      nil # nix
+      ruff # python
+      shellcheck # bash
+      rust-analyzer # rust
     ];
-    userKeymaps = { };
+    userKeymaps = [
+      {
+        context = "Workspace";
+        bindings = {
+          ctrl-tab = "workspace::ActivateNextPane";
+          ctrl-shift-tab = "workspace::ActivatePreviousPane";
+        };
+      }
+      {
+        context = "Editor";
+        bindings = {
+          f2 = "workspace::Save";
+          f3 = "editor::Rename";
+        };
+      }
+      {
+        context = "Pane";
+        bindings = { };
+      }
+    ];
     userSettings = {
+      lsp = {
+        nix.binary.path_lookup = true;
+        ruff.initialization_options.settings = {
+          lineLenght = 100;
+          lint = {
+            # default select = [ E4 E7 E9 F ]
+            extendSelect = [ "E" "W" "I" "N" "D" "UP" "ASYNC" "S" "BLE" "B" "A" "COM" "C4" "DTZ" "EM" "FA" "ISC" "ICN" "LOG" "G" "PIE" "PYI" "Q" "RSE" "RET" "SLF" "SIM" "TID" "TCH" "INT" "ARG" "TD" "FIX" "ERA" "PL" "TRY" "PERF" "FURB" "DOC" "RUF" ];
+            extendIgnore = [ "UP017" "UP032" ];
+          };
+          codeAction.fixViolation.enable = false;
+        };
+      };
+      language = {
+        Python = {
+          language_server = [ "ruff" ];
+          formatter = {
+            language_server.name = "ruff";
+            code_action = {
+              "source.organizeImports.ruff" = true;
+              "source.fixAll.ruff" = true;
+            };
+          };
+        };
+      };
+      language_model = {
+        ollama = {
+          api_url = "http://localhost:11434";
+          available_model = [
+            {
+              name = "";
+              display_name = "";
+              max_tokens = 32768;
+            }
+          ];
+        };
+      };
+      assistant = {
+        version = 2;
+        default_model = {
+          provider = "ollama";
+          model = "";
+        };
+      };
       vim_mode = true;
+      vim = {
+        use_system_clipboard = "on_yank";
+        use_smartcase_find = true;
+      };
+      relative_line_number = true;
+      scrollbar.show = "auto";
       telemetry.metrics = false;
+      load_direnv = "shell_hook";
+      auto_update = false;
+      format_on_save = "off";
+      hour_format = "hour24";
+      show_whitespaces = "boundary";
+      #soft_wrap = "bounded";
+      search.regex = true;
+      wrap_guides = [ 80 ];
       ui_font_size = 16;
       buffer_font_size = 16;
       theme = {
         mode = "system";
-        light = "One Light";
-        dark = "One Dark";
+        light = "Ayu Light";
+        dark = "Ayu Dark";
       };
     };
   };
