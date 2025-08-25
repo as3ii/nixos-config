032dfca463880e421bc4f913bd346c5c592fabae
as3ii <as3ii777@gmail.com>
2025-08-25 12:10:28 +0200

    Added mpv-hdr in the mpv home-manager module

--- home-manager/modules/mpv.nix
+++ home-manager/modules/mpv.nix
@@ -1,5 +1,13 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
+let
+  mpv-hdr = pkgs.writeShellScriptBin "mpv-hdr" ''
+    ENABLE_HDR_WSI=1 ${config.programs.mpv.finalPackage.outPath}/bin/mpv \
+       --vo=gpu-next --target-colorspace-hint \
+       --gpu-api=vulkan --gpu-context=waylandvk \
+       "$@"
+  '';
+in
 {
   programs.mpv = {
     enable = true;
@@ -24,4 +32,6 @@
     ];
     scriptOpts = { };
   };
+
+  home.packages = [ mpv-hdr ];
 }
