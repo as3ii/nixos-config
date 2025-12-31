56bc32ea76ca5f61bed8deca4c019762b9633ed6
as3ii <as3ii777@gmail.com>
2025-12-31 18:55:56 +0100

    Expanded audio module for more real-time work

--- modules/audio.nix
+++ modules/audio.nix
@@ -13,4 +13,31 @@
     pulse.enable = true;
     jack.enable = true;
   };
+  # Increase limits
+  security.pam.loginLimits = [
+    {
+      # Maximum locked-in-memory address space (KB)
+      domain = "@audio";
+      item = "memlock";
+      type = "-";
+      value = "unlimited";
+    }
+    {
+      # Maximum realtime priority
+      domain = "@audio";
+      item = "rtprio";
+      type = "-";
+      value = "90";
+    }
+  ];
+  # Allow set CPU DMA latency and kernel timers
+  services.udev = {
+    extraRules = ''
+      KERNEL=="rtc0", GROUP="audio"
+      KERNEL=="hpet", GROUP="audio"
+      DEVPATH=="/devices/virtual/misc/cpu_dma_latency", OWNER="root", GROUP="audio", MODE="0660"
+    '';
+  };
+  # Enable threaded IRQs
+  boot.kernelParams = [ "threadirqs" ];
 }
